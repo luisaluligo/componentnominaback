851dda5f15cf7eed499e8e268601dacb6b559f77
luisa fernanda r <luisaluligo@hotmail.com>
2022-07-05 23:50:41 -0500

    depuracion

--- authApp/serializers/tipoNovedadSerializer.py
+++ authApp/serializers/tipoNovedadSerializer.py
@@ -10,12 +10,12 @@ class TipoNovedadSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         tipoNovedadInstance = Tiponovedad.objects.create(**validated_data)
         return tipoNovedadInstance
-    
-    def update(self, instance, tipoNovedadInstance,validated_data):
+
+    def update(self,instance,validated_data):
        
-        tipoNovedadInstance.codigo_tipnov = validated_data.get('codigo_tipnov', instance.codigo_tipnov)
-        tipoNovedadInstance.nombre_tipnov = validated_data.get('nombre_tipnov', instance.nombre_tipnov)
-        tipoNovedadInstance.tipoconcepto_tipnov = validated_data.get('tipoconcepto_tipnov', instance.tipoconcepto_tipnov)
-        tipoNovedadInstance.valorbase_tipnov = validated_data.get('valorbase_tipnov', instance.valorbase_tipnov)
-        tipoNovedadInstance.save()
-        return tipoNovedadInstance
+        instance.codigo_tipnov = validated_data.get('codigo_tipnov', instance.codigo_tipnov)
+        instance.nombre_tipnov = validated_data.get('nombre_tipnov', instance.nombre_tipnov)
+        instance.tipoconcepto_tipnov = validated_data.get('tipoconcepto_tipnov', instance.tipoconcepto_tipnov)
+        instance.valorbase_tipnov = validated_data.get('valorbase_tipnov', instance.valorbase_tipnov)
+        instance.save()
+        return instance
--- authApp/views/tiponovedadUpdateView.py
+++ authApp/views/tiponovedadUpdateView.py
@@ -3,6 +3,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.backends import TokenBackend
 from rest_framework.permissions import IsAuthenticated
+from django.forms.models import model_to_dict
 
 from authApp.models.tiponovedad import Tiponovedad
 from authApp.serializers.tipoNovedadSerializer import TipoNovedadSerializer
@@ -12,15 +13,17 @@ class TiponovedadUpdateView(generics.UpdateAPIView):
      serializer_class= TipoNovedadSerializer
      permission_classes = (IsAuthenticated,)
 
-     def update(self, request, *args, **kwargs):
+     def put(self, request, *args, **kwargs):
 
         token = request.META.get('HTTP_AUTHORIZATION')[7:]
         tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
-        valid_data = tokenBackend.decode(token, verify=False)
-        
-        if valid_data['user_id'] != kwargs['user']:
-            stringResponse = {'detail': 'Unauthorized Request'}
-            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED) 
-        return super().update(request, *args, **kwargs)
+        valid_data = tokenBackend.decode(token,verify=False)
+        pk = kwargs["pk"]
+        instance = Tiponovedad.objects.get(pk=pk)
+        serializer = TipoNovedadSerializer(instance=instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.update(instance, validated_data=request.data)
+        return Response(model_to_dict(instance), status=status.HTTP_201_CREATED)
 
+        
        
