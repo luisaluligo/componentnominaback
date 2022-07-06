536201d6a7f1452ddcb05269f30fc79e6fd08a13
luisa fernanda r <luisaluligo@hotmail.com>
2022-07-05 22:51:13 -0500

    depuracion

--- authApp/views/tiponovedadUpdateView.py
+++ authApp/views/tiponovedadUpdateView.py
@@ -17,7 +17,7 @@ class TiponovedadUpdateView(generics.UpdateAPIView):
         token = request.META.get('HTTP_AUTHORIZATION')[7:]
         tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
         valid_data = tokenBackend.decode(token, verify=False)
-
+        return request
         if valid_data['user_id'] != kwargs['user']:
             stringResponse = {'detail': 'Unauthorized Request'}
             return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED) 
